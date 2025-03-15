5a32148b59b761a7dd998f9f4d4d8abb32673ab3
Seonjae Lee <seonjae9824@naver.com>
2025-03-15 16:19:03 +0900

    [level 1] Title: 평균 구하기, Time: 0.00 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
@@ -1,6 +1,3 @@
 def solution(arr):
-    num_sum = 0
-    for i in range(0, len(arr)):
-        num_sum += arr[i]
-        mean_arr = num_sum / len(arr)
-    return mean_arr
+    answer = sum(arr) / len(arr)
+    return answer
